5125d864d83a6367d5738b5b0dd8670a64266d3f
Vladimir Zubritsky <vladimir.zubritsky@origins-digital.com>
2022-03-24 15:28:37 +0300

    added custom component

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+CustomElement.init((element, _context) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
+
+CustomElement.setHeight(150);
+
